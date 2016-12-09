d33c17243983f83596bb377439bc3f30cfc68b74
John Flavin <flavinj@gmail.com>
2016-12-09 14:25:20 -0600

    Add: save json rep. of resource upon creation

--- src/main/java/org/nrg/containers/model/ContainerExecutionOutput.java
+++ src/main/java/org/nrg/containers/model/ContainerExecutionOutput.java
@@ -2,6 +2,7 @@ package org.nrg.containers.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.MoreObjects;
+import org.nrg.containers.model.xnat.XnatModelObject;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Transient;
@@ -17,6 +18,7 @@ public class ContainerExecutionOutput implements Serializable {
     @JsonProperty("parent") private String parentInputName;
     private String mount;
     private String path;
+    private String created;
 
     public ContainerExecutionOutput() {}
 
@@ -93,6 +95,14 @@ public class ContainerExecutionOutput implements Serializable {
         this.path = path;
     }
 
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(final String created) {
+        this.created = created;
+    }
+
     @Override
     public boolean equals(final Object o) {
         if (this == o) return true;
@@ -104,12 +114,13 @@ public class ContainerExecutionOutput implements Serializable {
                 Objects.equals(this.required, that.required) &&
                 Objects.equals(this.parentInputName, that.parentInputName) &&
                 Objects.equals(this.mount, that.mount) &&
-                Objects.equals(this.path, that.path);
+                Objects.equals(this.path, that.path) &&
+                Objects.equals(this.created, that.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, type, label, required, parentInputName, mount, path);
+        return Objects.hash(name, type, label, required, parentInputName, mount, path, created);
     }
 
     @Override
@@ -122,6 +133,7 @@ public class ContainerExecutionOutput implements Serializable {
                 .add("parent", parentInputName)
                 .add("mount", mount)
                 .add("path", path)
+                .add("created", created)
                 .toString();
     }
 }
